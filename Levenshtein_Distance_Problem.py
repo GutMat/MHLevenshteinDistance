25df1513057ee62e38662fa0a0d41d05afddd757
Gut Mateusz <s199693@student.uek.krakow.pl>
2020-10-19 15:00:46 +0200

    Update comments and some logic

--- Levenshtein_Distance_Problem.py
+++ Levenshtein_Distance_Problem.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
+
 # Funkcja obliczająca dystans Levenshteina
 # Przyjmuje 4 argumenty
 # Wyraz oryginalny oraz wyraz, który chcemy przeksztalcić
 # wraz ich długosciami
 # Taka implementacja tego problemu osiąga złożonosć czasową O(3^word2_len)
 def distance(word1, word1_len, word2, word2_len):
-    
     # Przypadek pierwszy
     # Sprawdzamy czy 1 wyraz jest pusty
     # Jesli jest pusty wymagana jest liczba operacji równa długosci drugiego wyrazu
@@ -32,7 +33,7 @@ def distance(word1, word1_len, word2, word2_len):
     # b) gdy dodajemy litere do pierwszego wyrazu ('ABC', 'ABD') => ('ABCD', 'ABD') == ('ABC', 'AB')
     # c) gdy zamieniamy ('ABC', 'ABD') => ('AB', 'AB')
     return min(distance(word1, word1_len - 1, word2, word2_len) + 1,			
-    			   distance(word1, word1_len, word2, word2_len - 1) + 1,			
+    			   distance(word1, word1_len, word2, word2_len - 1) + 1,		
     			   distance(word1, word1_len - 1, word2, word2_len - 1) + 1)
 
 # Funkcja obliczająca dystans Levenshteina wykorzystująca
@@ -46,24 +47,27 @@ def distanceDP(word1, word2):
     # Poniższa tablica przechowuje wszystkie możliwe wartosci odległosci pomiędzy
     # poszczególnymi znakami wyrazu pierwszego i wyrazu drugiego
     # Tablica posiada (word1_len +1)*(word2_len+1) wartosci
+    # Na poczatku inicjalizujemy ja samymi zerami
     memo = [[0 for word1 in range(word2_len + 1)] for word2 in range(word1_len + 1)]
+    
+    print("Tablica startowa (len(word1) + 1) na (len(word2) + 1) ")
+    print(np.array(memo))
 
-    # Następnie iterujemy po poszczególnych literach wyrazów porównując je
+    # Następnie iterujemy po poszczególnych literach (częsciach) wyrazów porównując je
     for i in range(word1_len +1):
         for j in range(word2_len + 1):
             
-            # Przypadek pierwszy
-            # Sprawdzamy czy 1 wyraz jest pusty
-            # Jesli jest pusty wymagana jest liczba operacji równa długosci drugiego wyrazu
+            # Inicjalizujemy wartosci w zerowym wierszu bazowym
+            # Uzupelniamy wartosciami od 0 do len(word1) + 1
             if i == 0: 
                 memo[i][j] = j
             
-            # Sprawdzamy czy 2 wyraz jest pusty
-            # Jesli jest pusty wymagana jest liczba operacji równa długosci drugiego wyrazu 
+                
+            # Inicjalizujemy wartosci w zerowej kolumnie bazowej
+            # Uzupelniamy wartosciami od 0 do len(word1) + 1
             elif j == 0: 
                 memo[i][j] = i
   
-            # Przypadek drugi
             # Sprawdzamy czy ostatnie znaki wyrazu są takie same
             # Jesli tak możemy pominąć ostatnie znaki i obliczyć dystans
             # dla pozostałych liter
@@ -71,17 +75,19 @@ def distanceDP(word1, word2):
                 memo[i][j] = memo[i-1][j-1] 
   
             # Jeśli ostatnie znaki się różnią, sprawdzamy wszystkie
-            # możliwości i wyciągamy wartość minimalną
+            # możliwości i wyciągamy wartość minimalną z podproblemu
             else: 
                 memo[i][j] = 1 + min(memo[i][j-1],      # Dodawanie
                                    memo[i-1][j],        # Usuwanie
                                    memo[i-1][j-1])      # Zamiana 
-  
-    return memo[word1_len][word2_len]
+    print("Tablica końcowa z wynikiem znajdującym się w prawym dolnym rogu memo[word1_len][word2_len]")            
+    print(np.array(memo))
+    
+    return print(memo[word1_len][word2_len])
 
 
-word_A = ""
-word_B = "wino"
+word_A = "kot"
+word_B = "płot"
 
 print(distance(word_A,len(word_A),word_B, len(word_B)))
-print(distanceDP(word_A, word_B))
+distanceDP(word_A, word_B)
